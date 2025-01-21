e1a3c907949edb831d68a816b6693baaf15db8b5
Juho Ström <juho.strom@tuni.fi>
2025-01-21 13:33:37 +0200

    Added Fibonacchi counter to TestScript

--- Code/Examples/TestScript.cs
+++ Code/Examples/TestScript.cs
@@ -4,15 +4,24 @@ using System;
 namespace SnakeGame;
 public partial class TestScript : Node
 {
+	private int _FiboSequence = 0;
+	private int _FiboNum1 = 0;
+	private int _FiboNum2 = 1;
+	private int _FrameCount = 2;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		GD.Print("Game started");
+		GD.Print($"Frame 1: {_FiboSequence}");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-
+		_FiboSequence = _FiboNum1 + _FiboNum2;
+		_FiboNum1 = _FiboNum2;
+		_FiboNum2 = _FiboSequence;
+		GD.Print($"Frame {_FrameCount}: {_FiboSequence}");
+		_FrameCount++;
 	}
 }
