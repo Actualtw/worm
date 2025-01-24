ec7daa6b79aa5aa3a528d951cbf679ed507e1270
Actual <actualtw@gmail.com>
2025-01-24 10:46:11 +0200

    Fixes to TestScript

--- Code/Examples/TestScript.cs
+++ Code/Examples/TestScript.cs
@@ -7,26 +7,32 @@ public partial class TestScript : Node
 	private int _FiboSequence = 0;
 	private int _FiboNum1 = 0;
 	private int _FiboNum2 = 1;
-	private int _FrameCount = 1;
+	private int _FrameCount = 0;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		GD.Print("Game started");
-		GD.Print($"Frame {_FrameCount}: {_FiboNum1}");
-		_FrameCount++;
-		GD.Print($"Frame {_FrameCount}: {_FiboNum2}");
-		_FrameCount++;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		if (_FrameCount == 0)
+		{
+			_FrameCount++;
+			GD.Print($"Frame {_FrameCount}: {_FiboNum1}");
+		}
+		if (_FrameCount == 1)
+		{
+			_FrameCount++;
+			GD.Print($"Frame {_FrameCount}: {_FiboNum2}");
+		}
 		if (_FiboSequence <= 1000) {
+			_FrameCount++;
 			_FiboSequence = _FiboNum1 + _FiboNum2;
 			_FiboNum1 = _FiboNum2;
 			_FiboNum2 = _FiboSequence;
 			GD.Print($"Frame {_FrameCount}: {_FiboSequence}");
-			_FrameCount++;
 		}
 	}
 }
