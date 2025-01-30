c19e5a47d43a2e08155071c81ccd2fbfcc4d7e6a
Actual <actualtw@gmail.com>
2025-01-30 20:01:21 +0200

    Character movement

--- Code/Examples/Snake.cs
+++ Code/Examples/Snake.cs
@@ -5,44 +5,46 @@ namespace SnakeGame;
 
 public partial class Snake : Sprite2D
 {
-	public int _speed = 50;
+	[Export]public float Speed;
+	public float _idle;
 	Vector2 _direction = new Vector2(1, 0);
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 	}
 
-	public void ReadInput()
+	public void Move()
 	{
 		if (Input.IsKeyPressed(Key.Right) || Input.IsKeyPressed(Key.D))
 		{
-			GD.Print("Suuntatoiminto oikea suoritettu");
 			_direction = new Vector2(1, 0);
+			_idle = Speed;
 		}
 
 		if (Input.IsKeyPressed(Key.Left) || Input.IsKeyPressed(Key.A))
 		{
-			GD.Print("Suuntatoiminto vasen suoritettu");
 			_direction = new Vector2(-1, 0);
+			_idle = Speed;
 		}
 
 		if (Input.IsKeyPressed(Key.Up) || Input.IsKeyPressed(Key.W))
 		{
-			GD.Print("Suuntatoiminto ylös suoritettu");
 			_direction = new Vector2(0, -1);
+			_idle = Speed;
 		}
 
 		if (Input.IsKeyPressed(Key.Down) || Input.IsKeyPressed(Key.S))
 		{
-			GD.Print("Suuntatoiminto alas suoritettu");
 			_direction = new Vector2(0, 1);
+			_idle = Speed;
 		}
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _PhysicsProcess(double delta)
 	{
-		ReadInput();
-		GlobalPosition += _direction * (float)delta * _speed;
+		_idle = 0;
+		Move();
+		GlobalPosition += _direction * (float)delta * _idle;
 	}
 }
