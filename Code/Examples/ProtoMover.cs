64836b0e72a59583c71fe044b8264de1597610ca
Juho Ström <juho.strom@tuni.fi>
2025-01-30 17:58:55 +0200

    Tuntitehtävä 2: Liikuta hahmoa kahden pisteen välillä

--- Code/Examples/ProtoMover.cs
+++ Code/Examples/ProtoMover.cs
@@ -1,18 +1,34 @@
 using Godot;
 using System;
+using System.Drawing;
 
 public partial class ProtoMover : Sprite2D
 {
-    [Export] private int _speed = 1;
-    [Export] private Vector2 _direction = new Vector2(0, 0);
+    [Export] Boolean MoveBetweenPoints;
+    [Export] private int _speed;
+    [Export] private Vector2 _direction;
+    [Export] private Vector2 StartPosition;
+    [Export] private Vector2 TargetPosition;
+
+    private Vector2 TempTarget;
     public override void _Ready()
     {
-
+        Position = StartPosition;
+        TempTarget = TargetPosition;
     }
 
     public override void _Process(double delta)
     {
+        if (MoveBetweenPoints)
+        {
+            var directionToTarget = (TempTarget - Position).Normalized();
+            Position += directionToTarget * (float)delta * _speed;
 
-        GlobalPosition += _direction * (float)delta * _speed;
+            if (Position.DistanceTo(TempTarget) < 1)
+            {
+                TempTarget = TempTarget == TargetPosition ? StartPosition : TargetPosition;
+            }
+        }
+            GlobalPosition += _direction * (float)delta * _speed;
     }
 }
